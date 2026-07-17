90a49ebce27c4839b37f96b2c380e1a4fb0c43c6
Arshad <arshadhm200@gmail.com>
2026-07-17 22:20:58 +0530

    Modified Render Creds

--- Backend/src/main/java/com/infy/newgen/service/EmailServiceImpl.java
+++ Backend/src/main/java/com/infy/newgen/service/EmailServiceImpl.java
@@ -17,10 +17,10 @@ import com.infy.newgen.dto.PaymentReminderDTO;
 @Transactional
 public class EmailServiceImpl implements EmailService {
 
-    @Value("${spring.mail.password}") // Maps your SendGrid API key secret
+    @Value("${MAIL_PASSWORD}") // Maps your SendGrid API key secret
     private String sendGridApiKey;
 
-    @Value("${spring.mail.username}") // Maps your verified SendGrid sender email
+    @Value("${MAIL_USER}") // Maps your verified SendGrid sender email
     private String fromEmail;
 
     // Create a single shared instance of RestClient pointing to SendGrid
